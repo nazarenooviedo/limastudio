20644a93b51e86d39125e88b29bbf457f5c20987
Nazareno Oviedo <nazareno@basement.studio>
2023-02-16 00:09:12 -0300

    update dots color

--- src/lib/gsap/index.tsx
+++ src/lib/gsap/index.tsx
@@ -6,9 +6,6 @@ gsap.registerPlugin(CustomEase)
 const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2
 const RECIPROCAL_GR = 1 / GOLDEN_RATIO
 const DURATION = RECIPROCAL_GR
-const OUT_DURATION = DURATION
-const DELAY_TRANSITION_DURATION = DURATION * 0.6
-const CUSTOM_EASE = CustomEase.create('EaseIn', '0.165, 0.84, 0.44, 1')
 const EASE_LAYER = CustomEase.create(
   'EASE_LAYER',
   'M0,0 C0.268,0.43 0.41,1 1,1'
@@ -19,19 +16,9 @@ gsap.config({
   nullTargetWarn: false
 })
 
-gsap.ticker.lagSmoothing(100, 300)
-
 gsap.defaults({
   duration: DURATION,
   ease: EASE_LAYER
 })
 
-export {
-  CUSTOM_EASE,
-  DELAY_TRANSITION_DURATION,
-  DURATION,
-  EASE_LAYER,
-  GOLDEN_RATIO,
-  gsap,
-  OUT_DURATION
-}
+export { DURATION, EASE_LAYER, GOLDEN_RATIO, gsap }
